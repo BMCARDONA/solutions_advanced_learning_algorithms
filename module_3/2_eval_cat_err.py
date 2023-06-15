0b748c82706bc7cf08ae8e53df486c843c8a344c
Bradley <bradleymathematics@gmail.com>
2023-06-15 15:33:50 -0400

    add solution

---
+++ module_3/2_eval_cat_err.py
@@ -0,0 +1,22 @@
+# UNQ_C2
+# GRADED CELL: eval_cat_err
+def eval_cat_err(y, yhat):
+    """ 
+    Calculate the categorization error
+    Args:
+      y    : (ndarray  Shape (m,) or (m,1))  target value of each example
+      yhat : (ndarray  Shape (m,) or (m,1))  predicted value of each example
+    Returns:|
+      cerr: (scalar)             
+    """
+    m = len(y)
+    incorrect = 0
+    for i in range(m):
+        
+    ### START CODE HERE ### 
+        if y[i] != yhat[i]:
+            incorrect += 1
+    cerr = (incorrect / m)
+    ### END CODE HERE ### 
+    
+    return(cerr)
