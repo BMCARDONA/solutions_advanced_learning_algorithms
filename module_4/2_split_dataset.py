a2840c32d1fc9c535da2b2939e618d649f0f80c1
Bradley <bradleymathematics@gmail.com>
2023-06-16 18:22:09 -0400

    add solution

---
+++ module_4/2_split_dataset.py
@@ -0,0 +1,31 @@
+# UNQ_C2
+# GRADED FUNCTION: split_dataset
+
+def split_dataset(X, node_indices, feature):
+    """
+    Splits the data at the given node into
+    left and right branches
+    
+    Args:
+        X (ndarray):             Data matrix of shape(n_samples, n_features)
+        node_indices (list):     List containing the active indices. I.e, the samples being considered at this step.
+        feature (int):           Index of feature to split on
+    
+    Returns:
+        left_indices (list):     Indices with feature value == 1
+        right_indices (list):    Indices with feature value == 0
+    """
+    
+    # You need to return the following variables correctly
+    left_indices = []
+    right_indices = []
+    
+    ### START CODE HERE ###
+    for node_index in node_indices:
+        if X[node_index][feature] == 1:
+            left_indices.append(node_index)
+        else:
+            right_indices.append(node_index)            
+    ### END CODE HERE ###
+        
+    return left_indices, right_indices
